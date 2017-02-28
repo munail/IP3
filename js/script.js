458786675ab64bd21147494c3ed26e7b8989f9bd
munail <munailahmed003@gmail.com>
2017-02-28 18:25:12 +0300

    hektik

--- js/script.js
+++ js/script.js
@@ -1,33 +1,28 @@
-//Business Logic
-var range = [];
-
-function pingPong(userInput){
-	for(i= 1;i<= userInput;i++){
-		range.push(i);
-	}
-
-	for(index=0;index<=range.length;index++){
-		if(range[index]%15 === 0) {
-			range.splice(index, 1, "pingpong");
-		} else if(range[index]%5 === 0) {
-			range.splice(index, 1, "pong");
-		} else if(range[index]%3 === 0) {
-			range.splice(index, 1, "ping");
-		};
-	};
-}
-
-//UI
-$(document).ready(function() {
-	$("form").submit(function(event){
-		event.preventDefault();
-		var userInput = $("#input").val();
-
-		pingPong(userInput);
-		for (i = 0; i<=range.length; i++){
-      	$("ul#output").append("<li >"+range[i]+"</li>");
-      }
-
-    var userInput = $("#input").val("");
-	});
+//Business logic
+var pingPong = function (userInput) {
+    var result = [];
+    for (var x = 1; x <= userInput; x++) {
+        if (x % 15 === 0) {
+            result.push("pingpong");
+        } else if (x % 5 === 0) {
+            result.push("pong");
+        } else if (x % 3 === 0) {
+            result.push("ping");
+        } else {
+            result.push(x);
+        }
+    }
+    return result;
+};
+//user interface
+$(document).ready(function () {
+    $("form#forminput").submit(function (event) {
+        event.preventDefault();
+        $("ul#output").empty();
+        var userInput = parseInt($("input#number").val());
+        var result = pingPong(userInput);
+        result.forEach(function (number) {
+            $("ul#output").append('<li>' + number + '</li>');
+        });
+    });
 });
